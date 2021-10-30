cfde7d0d94840a4f56ad266404e291cf388c7198
Michael McDonagh <mikemcdonagh@protonmail.com>
2021-10-30 15:54:21 -0400

    Added input and output options
    Input can come from file or stdin
    Output can be HTML or Gemtext

--- src/main.go
+++ src/main.go
@@ -7,22 +7,37 @@ import (
 )
 
 var templateFile string
+var html bool
+var gemtext bool
 
 func init() {
-	flag.StringVar(&templateFile, "t", "test.gmi",
-		"Gemtext-Like file to convert to HTML, Gemtext, and/or PDF")
+	flag.StringVar(&templateFile, "t", "",
+		"Gemtext-Like file to convert to HTML, Gemtext, and/or PDF. Exclude to read from STDIN")
+
+	flag.BoolVar(&html, "h", false, "Convert to HTML and output to STDOUT")
+	flag.BoolVar(&gemtext, "g", false, "Convert to Gemtext and output to STDOUT")
 }
 
 func main() {
 	flag.Parse()
 
-	f, err := os.Open(templateFile)
-	if err != nil {
-		panic(err)
-	}
-	defer f.Close()
+	var m markup.Markups
+	if templateFile == "" {
+		m = markup.ParseFromGemtext(os.Stdin)
+	} else {
+		f, err := os.Open(templateFile)
+		if err != nil {
+			panic(err)
+		}
+		defer f.Close()
 
-	m := markup.ParseFromGemtext(f)
-	m.Gemtext(os.Stdout)
-	m.HTML(os.Stdout)
+		m = markup.ParseFromGemtext(f)
+	}
+
+	if html {
+		m.HTML(os.Stdout)
+	}
+	if gemtext {
+		m.Gemtext(os.Stdout)
+	}
 }
